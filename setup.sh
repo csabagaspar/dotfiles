8ffb83a9b1b905de57345efb16e19784ae58f723
csabagaspar <gaspar.csaba@gmail.com>
2016-09-18 10:38:01 +0200

    init

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo ".files..."
+if [[ ! -d $HOME/.files ]]; then
+  git clone https://github.com/csabagaspar/.files.git $HOME/.files
+else
+  cd $HOME/.files && git pull --rebase
+fi
+
+cd $HOME
+
+echo "Linking dotfile..."
+ln -fs .files/.vimrc
+
+echo "Git config..."
+#sh $HOME/.files/gitconfig.sh
+
+echo "Done."
