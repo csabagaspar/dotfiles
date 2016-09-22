7c9628cf05d142639a03a63b67467a16b501ba66
csgaspar <csaba.gaspar@nokia.com>
2016-09-22 09:20:22 +0200

    updated git config

--- gitconfig.sh
+++ gitconfig.sh
@@ -1,10 +1,10 @@
-git config --global alias.a add
-git config --global alias.b branch
-git config --global alias.co checkout
-git config --global alias.st status
-git config --global alias.c commit
+git config --global alias.a "add"
+git config --global alias.b "branch"
+git config --global alias.co "checkout"
+git config --global alias.st "status"
+git config --global alias.c "commit"
 
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %Cblue<%an>%Creset' --abbrev-commit --date=relative"
 git config --global alias.wslog "log --ignore-space-at-eol -p -w -b"
 git config --global alias.wsdiff "diff --ignore-space-at-eol -w -b --no-ext-diff"
-git config --global core.editor gvim
+git config --global core.editor "gvim"
