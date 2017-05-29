5793df580179185bee8ec4b24cb87171031ad849
csabagaspar <gaspar.csaba@gmail.com>
2017-05-29 12:18:36 +0100

    updated tmux

--- tmux/start.sh
+++ tmux/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
-tmux new -s main -n workspace
-tmux neww  # Creates a 2nd window, in addition to the new session's first window
+tmux new-session -s main \; split-window -v \; detach \;
+tmux new-session -s katas \; send-keys 'cd ~/workspace/github/katas/javascript' C-m \; send-keys 'vim' C-m \; split-window -v -p 20 \; send-keys 'cd ~/workspace/github/katas/javascript' C-m \; send-keys 'npm run test:watch' \; detach \;
+tmux new-session -s dotfiles \; send-keys 'cd ~/dotfiles' C-m \; send-keys 'vim' C-m \; detach \;
 tmux attach -t main
