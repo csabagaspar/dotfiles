0be3217a778492ace0b646451ed811a1e19e2e55
csabagaspar <gaspar.csaba@gmail.com>
2017-06-09 09:11:56 +0100

    updated install

--- bash/alias.bash
+++ bash/alias.bash
@@ -1,4 +1,4 @@
 alias gitignore='cp ~/dotfiles/git/.gitignore .'
 alias gitattributes='cp ~/dotfiles/git/.gitattributes .'
 alias editorconfig='cp ~/dotfiles/editorconfig/.editorconfig .'
-alias tern-project='cp ~/dotfiles/tern/.tern-project .'
+alias tern-project='cp ~/dotfiles/tern/tern-project.symlink ./.tern-project'
---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+echo "Installing dotfiles."
+
+echo "Initializing submodule(s)"
+git submodule update --init --recursive
+
+DOTFILES=$HOME/dotfiles
+
+echo -e "\nCreating symlinks"
+echo "=============================="
+linkables=$( find -H "$DOTFILES" -maxdepth 3 -name '*.symlink' )
+for file in $linkables ; do
+    target="$HOME/.$( basename $file '.symlink' )"
+    if [ -e $target ]; then
+        echo "~${target#$HOME} already exists... Skipping."
+    else
+        echo "Creating symlink for $file"
+        ln -s $file $target
+    fi
+done
+echo -e "\n\nCreating vim symlinks"
+echo "=============================="
+VIMFILES=( "$HOME/.vim:$DOTFILES/vim"
+        "$HOME/.vimrc:$DOTFILES/vim/vimrc" )
+
+for file in "${VIMFILES[@]}"; do
+    KEY=${file%%:*}
+    VALUE=${file#*:}
+    if [ -e ${KEY} ]; then
+        echo "${KEY} already exists... skipping."
+    else
+        echo "Creating symlink for $KEY"
+        ln -s ${VALUE} ${KEY}
+    fi
+done
+
+
+echo "Running git setup..."
+sh ~/dotfiles/git/gitconfig.sh
+
+echo "Done."
