5c79fba659dae04fb18a9ecd4d8ae22e2fdd6361
csabagaspar <gaspar.csaba@gmail.com>
2018-01-30 09:03:52 +0000

    update docs

--- bash/proxy-switch.sh
+++ bash/proxy-switch.sh
@@ -5,13 +5,13 @@ set -o errexit
 HOST="10.144.1.10"
 PORT=8080
 if [ $1 == "on" ]; then
-	eval `export http_proxy=http://$HOST:$PORT`
-	eval `npm config set proxy http://$HOST:$PORT`
-	eval `git config --global http.proxy http://$HOST:$PORT`
-	echo Proxy Set
+  eval `export http_proxy=http://$HOST:$PORT`
+  eval `npm config set proxy http://$HOST:$PORT`
+  eval `git config --global http.proxy http://$HOST:$PORT`
+  echo Proxy Set
 else
-	eval `unset http_proxy`
-	eval `npm config delete proxy`
-	eval `git config --global --unset core.gitproxy`
-	echo Proxy Unset
+  eval `unset http_proxy`
+  eval `npm config delete proxy`
+  eval `git config --global --unset core.gitproxy`
+  echo Proxy Unset
 fi
