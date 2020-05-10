344a2a8a31ba55ec43929e7e4f526246d1f72783
csabagaspar <gaspar.csaba@gmail.com>
2020-05-10 13:44:41 +0200

    add typescriptreact ctags

--- apps/ctags/install.sh
+++ apps/ctags/install.sh
@@ -11,5 +11,5 @@ if [ -e $HOME/.ctags ]; then
   echo ".ctags already exists... skipping."
 else
   echo "Creating symlink for .ctags"
-  ln -s $DOTFILES/submodules/typescript-ctags.git/.ctags  $HOME/.ctags
+  ln -s $DOTFILES/submodules/typescriptreact-ctags.git/.ctags  $HOME/.ctags
 fi
