d3b1ec51afba7ae200dd8d72c130ede35aa1135a
csabagaspar <gaspar.csaba@gmail.com>
2020-05-10 08:38:55 +0200

    add tagbar vim plugin

--- apps/vim/install.sh
+++ apps/vim/install.sh
@@ -27,6 +27,7 @@ VIMFILES=( "$HOME/.vim:$APPS/vim"
         "$HOME/.vim/bundle/tmux-polyglot:$DOTFILES/submodules/vim-polyglot.git"
         "$HOME/.vim/bundle/vim-colorschemes:$DOTFILES/submodules/vim-colorschemes.git"
         "$HOME/.vim/bundle/vim-misc:$DOTFILES/submodules/vim-misc.git"
+        "$HOME/.vim/bundle/tagbar:$DOTFILES/submodules/tagbar.git"
 	"$HOME/.vim/bundle/vim-notes:$DOTFILES/submodules/vim-notes.git" )
 
 
