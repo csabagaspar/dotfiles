969e356143e007c421f68a152870f6df7a0e42b5
Csaba Gaspar <cgaspar@accretivetg.com>
2021-01-18 10:14:02 +0100

    a

--- apps/git/gitconfig.sh
+++ apps/git/gitconfig.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-git config --global user.name "csabagaspar"
-git config --global user.email "gaspar.csaba@gmail.com"
+git config --global user.name "Csaba Gaspar"
+git config --global user.email "cgaspar@accretivetg.com"
 git config --global core.longpaths "true"
 git config --global push.default "upstream"
 git config --global core.excludesfile "$HOME/.gitignore_global"
