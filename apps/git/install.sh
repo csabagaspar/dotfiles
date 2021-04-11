a0ffe185d201fd40677b06318c5ae7f6750f3e48
Csaba Gaspar <cgaspar@accretivetg.com>
2021-04-11 15:31:12 +0200

    update g

--- apps/git/install.sh
+++ apps/git/install.sh
@@ -3,3 +3,4 @@ echo "=============================="
 echo "Running git setup..."
 echo "=============================="
 /bin/bash ~/dotfiles/apps/git/gitconfig.sh
+/bin/bash ~/dotfiles/apps/git/gitconfig-local.sh
