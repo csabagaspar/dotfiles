43c113fd8e7aeaee1c552c2b881dd9f8a8f24aeb
Csaba Gaspar <cgaspar@accretivetg.com>
2024-02-29 09:27:09 +0100

    update

--- ubuntu.sh
+++ ubuntu.sh
@@ -36,9 +36,9 @@ else
   echo -e "\n\nInstalling non apt packages..."
   echo "=============================="
 
-  /bin/bash sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-  /bin/bash sudo nvm install 10.12.0 64
-  /bin/bash sudo apt-get install --no-install-recommends yarn
+  #/bin/bash sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+  #/bin/bash sudo nvm install 10.12.0 64
+  #/bin/bash sudo apt-get install --no-install-recommends yarn
   #fzf manual install
 
 fi
