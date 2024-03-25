c86c2c8c3f504964d890c6ac567796ad7a63dc7a
Csaba Gaspar <cgaspar@accretivetg.com>
2024-03-25 11:54:58 +0100

    update

--- apps/git/gitconfig-local.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-git config --global user.name "Csaba Gaspar"
-git config --global user.email "cgaspar@accretivetg.com"
--- apps/git/install.bash.sh
+++ apps/git/install.bash.sh
@@ -3,4 +3,4 @@ echo "=============================="
 echo "Running git setup..."
 echo "=============================="
 /bin/bash ~/dotfiles/apps/git/gitconfig.sh
-/bin/bash ~/dotfiles/apps/git/gitconfig-local.sh
+/bin/bash ~/dotfiles/apps/git/gitconfig.local.sh
