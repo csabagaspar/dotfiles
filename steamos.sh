970ee2c2b57319e5e23747239e33371b31efa8f8
Csaba Gaspar <cgaspar@accretivetg.com>
2024-04-06 11:02:52 +0200

    update

--- steamos.sh
+++ steamos.sh
@@ -5,7 +5,7 @@ sudo steamos-readonly disable
 
 #pacman
 sudo pacman-key --init # run these two commands if the below doesn't work
-sudo pacman-key --populate archlinux
+sudo pacman-key --populate
 
 installs=$( find -H "$HOME/dotfiles" -maxdepth 3 -name 'install.steamos.sh' )
  for file in $installs ; do
